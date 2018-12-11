269dfdbbbacafcdc309ec02afa40ab6c7dadc27d
Barry Lu <alijk0803@126.com>
2018-12-11 22:57:30 +0800

    sample应用上传

---
+++ cloud-application/src/main/java/com/function/configure/InjectDIYBeanConfigure.java
@@ -0,0 +1,12 @@
+package com.function.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InjectDIYBeanConfigure {
+	@Bean
+	public Object getBean() {
+		return null;
+	}
+}
